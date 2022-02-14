df5a463e1b41b8aae33ff4118d3f04095906fb32
Mister-Stalker <pna24052006@yandex.ru>
2022-02-14 12:58:34 +0300

    Update npc.py

--- files/scripts/npc.py
+++ files/scripts/npc.py
@@ -5,7 +5,7 @@ from bson.objectid import ObjectId
 from files.scripts import items
 
 
-class User:
+class NPC:
     def __init__(self, _id: ObjectId):
         self._id = _id
         client = pymongo.MongoClient("localhost", 27017)
@@ -13,13 +13,13 @@ class User:
         self.data = self.collection.find_one({"_id": self._id})
         self.is_reg = bool(self.data)
 
-    def get_data(self):
+    def get_data(self) -> dict:
         return self.collection.find_one({"_id": self._id})
 
-    def update(self, key, value):
+    def update(self, key, value) -> None:
         self.collection.update_one({"_id": self._id}, {'$set': {key: value}})
 
-    def add_to_inventory_stackable(self, tpl, count=1):
+    def add_to_inventory_stackable(self, tpl, count=1) -> None:
         self.get_data()
         if not items.get_info_for_tpl(tpl)["stackable"]:
             return
@@ -39,16 +39,16 @@ class User:
             self.collection.update_one({"_id": self._id}, {'$pull': {"inventory": {"tpl": tpl}}})
             self.collection.update_one({"_id": self._id}, {'$push': {"inventory": item}})
 
-    def get_from_inventory_stackable(self, tpl):
+    def get_from_inventory_stackable(self, tpl) -> dict:
         self.get_data()
         if not items.get_info_for_tpl(tpl)["stackable"]:
             return
         for i, item in enumerate(self.data["inventory"]):
             if item["tpl"] == tpl:
                 return item
-        return None
+        return {}
 
-    def remove_from_inventory_stackable(self, tpl, count):
+    def remove_from_inventory_stackable(self, tpl, count) -> bool:
         self.get_data()
         if not items.get_info_for_tpl(tpl)["stackable"]:
             return
@@ -63,11 +63,9 @@ class User:
                 print(item)
                 self.collection.update_one({"_id": self._id}, {'$pull': {"inventory": {"tpl": tpl}}})
                 self.collection.update_one({"_id": self._id}, {'$push': {"inventory": item}})
-        if flag:
-            return "no obj"
-        return "del"
+        return not flag
 
-    def get_from_inventory(self, item_id):
+    def get_from_inventory(self, item_id) -> list:
         self.get_data()
         return list(filter(lambda x: not x["stackable"] and x["_id"] == item_id, self.data["inventory"]))[0]
 
@@ -81,10 +79,10 @@ class User:
                 _items.append(item)
         return _items
 
-    def add_to_inventory(self, item):
+    def add_to_inventory(self, item) -> None:
         self.collection.update_one({"_id": self._id}, {'$push': {"inventory": item}})
 
-    def remove_from_inventory(self, item_id):
+    def remove_from_inventory(self, item_id) -> None:
         self.collection.update_one({"_id": self._id}, {'$pull': {"inventory": {"_id": ObjectId(item_id)}}})
 
     def __getitem__(self, item):
