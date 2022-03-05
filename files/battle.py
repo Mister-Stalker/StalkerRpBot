d14ddab7c1296028dff68650e683a4f69a94be60
Mister-Stalker <pna24052006@yandex.ru>
2022-03-05 12:53:17 +0300

    Create battle.py

---
+++ files/battle.py
@@ -0,0 +1,24 @@
+class Battle:
+    battle_list = {}
+    
+    def __init__(self, loc):
+        self.location = loc
+        
+    @staticmethod
+    def create_battle(loc, players: list, main_player: int, npc: list):
+        Battle.battle_list[loc] = {
+            "npc": npc,  # список _id нпс 
+            "players": players,  # список id игроков
+            "start_players": players,  # список id игроков которые были в начале
+            "main_player": main_player  # id "главного" игрока
+        }
+        
+    async def run(self):
+        
+        pass
+        
+    def __getitem__(self, item):
+        return self.battle_list[self.location][item]
+    
+    def data(self):
+        return self.battle_list[self.location]
